a388495f87368eae9eea54d59e8c7712d247a916
Andrew Kelly <andrew_kelly_2004@hotmail.com>
2022-06-29 15:05:29 +0000

    typo in messages.storage in settings file

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -94,7 +94,7 @@ TEMPLATES = [
     },
 ]
 
-MESSAGE_STORAGE = 'django.contrib.messages.storeage.session.SessionStorage'
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
